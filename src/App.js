3724812317a65ea15af1e9679ba015c2efe05f85
julio <julioakzzo@hotmail.com>
2021-09-07 13:54:06 -0300

    the header implementation and the complete principal layout

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow'
 import './App.css'
 import FeaturedMovie from './components/FeaturedMovie'
+import Header from './components/Header/Header'
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
 
   useEffect(() => {
     const loadAll = async () => {
@@ -27,20 +29,44 @@ export default () => {
     loadAll();
   }, []);
 
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener);
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    }
+  }, [])
+
   return (
     <div className="page">
+      <Header black={blackHeader} />
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData} />
+      }
+
       <section className="lists">
-
-        {featuredData &&
-          <FeaturedMovie item={featuredData} />
-        }
-
         {movieList.map((item, key) => (
           <div>
             <MovieRow key={key} title={item.title} items={item.items} />
           </div>
         ))}
       </section>
+
+      <footer>
+        Feito com <span role="img" aria-label="coracao"></span> por júlio Alves inspirado no video do Bonieky Lacerda <br />
+        Direitos de imagem para a netflix <br />
+        Dados do site themoviedb.org
+      </footer>
+
     </div>
   );
 }
